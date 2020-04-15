684aa156e83eed57ef016e06c382055ab59168c2
Kento Kawakami <emaxser@bonprosoft.com>
2020-04-16 07:33:32 +0900

    Support go module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kimitoboku/eleking
+
+go 1.12
+
+require github.com/urfave/cli v1.22.4
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"os"
 	"strings"
 
-	"github.com/codegangsta/cli"
+	"github.com/urfave/cli"
 	"github.com/kimitoboku/eleking/tldr"
 )
 
